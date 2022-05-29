a8d582a636044cc6dd28e8d844e1aa07714bde24
CHO CHAN HEE <cksgml4424@gmail.com>
2022-05-29 16:04:50 +0900

    connectionPool test case commit

--- src/test/java/hello/jdbc/connection/ConnectionTest.java
+++ src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -1,5 +1,6 @@
 package hello.jdbc.connection;
 
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -26,10 +27,24 @@ public class ConnectionTest {
     void dataSourceDriverManger() throws SQLException {
         // DriverManagerDataSource - 항상 새로운 커넥션을 획득
         DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
-        userDataSource(dataSource);
+        useDataSource(dataSource);
     }
 
-    private void userDataSource(DataSource dataSource) throws SQLException {
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        // 커넥션 풀링
+        HikariDataSource datasource = new HikariDataSource();
+        datasource.setJdbcUrl(URL);
+        datasource.setUsername(USERNAME);
+        datasource.setPassword(PASSWORD);
+        datasource.setMaximumPoolSize(10);
+        datasource.setPoolName("MyPool");
+
+        useDataSource(datasource);
+        Thread.sleep(1000);
+    }
+
+    private void useDataSource(DataSource dataSource) throws SQLException {
         Connection con1 = dataSource.getConnection();
         Connection con2 = dataSource.getConnection();
         log.info("connection={}, class={}", con1, con1.getClass());
